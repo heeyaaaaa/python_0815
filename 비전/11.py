9ffe7f45e27c4ecc6ef48541365930f1eef19df0
Sohee Hwang <hsh95815@naver.com>
2020-02-17 10:15:43 +0900

    200217_3

---
+++ 비전/11.py
@@ -0,0 +1,73 @@
+import numpy as np
+import cv2
+
+def bluring():
+    img = cv2.imread('model.png')
+    
+    kernel = np.ones((5,5), np.float32)/25
+    blur = cv2.filter2D(img, -1, kernel)
+    
+    cv2.imshow('original', img)
+    cv2.imshow('blur', blur)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+#bluring()
+    
+
+def onMouse(x):
+    pass
+
+def bluring2():
+    img = cv2.imread('model.png')
+    
+    cv2.namedWindow('BlurPane')
+    cv2.createTrackbar('BLUR_MODE', 'BlurPane', 0,2,onMouse)
+    cv2.createTrackbar('BLUR', 'BlurPane', 0, 5, onMouse)
+    
+    mode = cv2.getTrackbarPos('BLUR_MODE', 'BlurPane')
+    val = cv2.getTrackbarPos('BLUR', 'BlurPane')
+    
+    while True:
+        val = val*2 + 1
+        
+        try:
+            if mode == 0:
+                blur = cv2.blur(img, (val,val))
+            elif mode == 1:
+                blur = cv2.GaussianBlur(img, (val, val), 0)
+            elif mode == 2:
+                blur = cv2.medianBlur(img, val)
+            else:
+                break
+            
+            cv2.imshow('BlurPane', blur)
+        except:
+            break
+        
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+        
+        mode = cv2.getTrackbarPos('BLUR_MODE', 'BlurPane')
+        val = cv2.getTrackbarPos('BLUR', 'BlurPane')
+    cv2.destroyAllWindows()
+    
+#bluring2()
+
+#bilateral 필터
+    
+def bluring3():
+    img = cv2.imread('model.png')
+    
+    bilateral_blur = cv2.bilateralFilter(img, 9, 75, 75)
+    
+    cv2.imshow('original', img)
+    cv2.imshow('bilateral_blur', bilateral_blur)
+    
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+bluring3()
+
